8c6448425ec7930ea3607d7d84b8bebab506a400
zlove.zhang <zlove.zhang@bytedance.com>
2021-08-03 17:53:05 +0800

    new study: invert tree node

---
+++ src/com/zlove/practice/array/SingleNumber.java
@@ -0,0 +1,12 @@
+package com.zlove.practice.array;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+        int single = 0;
+        for (int num : nums) {
+            single ^= num;
+        }
+        return single;
+    }
+}
