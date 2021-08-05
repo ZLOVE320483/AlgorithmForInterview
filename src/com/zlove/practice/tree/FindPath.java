f255b55f358c589cf609af77df97b00baa579502
zlove.zhang <zlove.zhang@bytedance.com>
2021-08-05 17:04:50 +0800

    new study FindPath

---
+++ src/com/zlove/practice/tree/FindPath.java
@@ -0,0 +1,56 @@
+package com.zlove.practice.tree;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 输入一棵二叉树和一个整数，打印出二叉树中节点值的和为输入整数的所有路径。从树的根节点开始往下一直到叶节点所经过的节点形成一条路径。
+ *
+ *  
+ *
+ * 示例:
+ * 给定如下二叉树，以及目标和 target = 22，
+ *               5
+ *              / \
+ *             4   8
+ *            /   / \
+ *           11  13  4
+ *          /  \    / \
+ *         7    2  5   1
+ *
+ * 返回
+ *  [
+ *    [5,4,11,2],
+ *    [5,8,4,5]
+ * ]
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class FindPath {
+
+    private List<List<Integer>> res = new ArrayList<>();
+
+    public List<List<Integer>> findPath(TreeNode root, int target) {
+        backTrace(root, target, new ArrayList<>());
+        return res;
+    }
+
+    private void backTrace(TreeNode root, int target, List<Integer> path) {
+        if (root == null) {
+            return;
+        }
+        path.add(root.val);
+        target -= root.val;
+        if (target == 0 && root.left == null && root.right == null) {
+            res.add(new ArrayList<>(path));
+        }
+        backTrace(root.left, target, path);
+        backTrace(root.right, target, path);
+        path.remove(path.size() - 1);
+    }
+}
