2d99bf2a626d92991376d8b3ac4fa6ee72ba2c60
zlove.zhang <zlove.zhang@bytedance.com>
2021-08-09 14:46:43 +0800

    new study: 二叉树直径

---
+++ src/com/zlove/practice/tree/DiameterOfBinaryTree.java
@@ -0,0 +1,24 @@
+package com.zlove.practice.tree;
+
+/**
+ * 给定一棵二叉树，你需要计算它的直径长度。一棵二叉树的直径长度是任意两个结点路径长度中的最大值。这条路径可能穿过也可能不穿过根结点。
+ */
+public class DiameterOfBinaryTree {
+
+    int res = 0;
+
+    public int diameterOfBinaryTree(TreeNode root) {
+        depth(root);
+        return res - 1;
+    }
+
+    private int depth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        int left = depth(root.left);
+        int right = depth(root.right);
+        res = Math.max(res, left + right + 1);
+        return Math.max(left, right) + 1;
+    }
+}
