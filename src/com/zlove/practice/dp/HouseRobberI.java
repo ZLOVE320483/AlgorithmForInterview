1bf34958eae7062a46acd960c4bbb6af0fae0649
zlove.zhang <zlove.zhang@bytedance.com>
2021-08-11 17:46:06 +0800

    new study: 打家劫舍2

--- src/com/zlove/practice/dp/HouseRobberI.java
+++ src/com/zlove/practice/dp/HouseRobberI.java
@@ -37,4 +37,23 @@ public class HouseRobberI {
         }
         return dp[nums.length - 1];
     }
+
+    public int rob2(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        if (nums.length == 2) {
+            return Math.max(nums[0], nums[1]);
+        }
+        int first = nums[0], second = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < nums.length; i++) {
+            int tmp = second;
+            second = Math.max(second, first + nums[i]);
+            first = tmp;
+        }
+        return second;
+    }
 }
