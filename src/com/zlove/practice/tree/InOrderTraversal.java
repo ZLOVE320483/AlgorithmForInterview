1a052564762b8318a4bbe547cd8f987549010b81
zlove.zhang <zlove.zhang@bytedance.com>
2021-08-19 20:19:25 +0800

    new study: 二叉树中序遍历的两种方式

---
+++ src/com/zlove/practice/tree/InOrderTraversal.java
@@ -0,0 +1,54 @@
+package com.zlove.practice.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 给定一个二叉树的根节点 root ，返回它的 中序 遍历。
+ */
+public class InOrderTraversal {
+
+    /**
+     * 递归方式
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        inOrder(root, res);
+        return res;
+    }
+
+    private void inOrder(TreeNode node, List<Integer> list) {
+        if (node == null) {
+            return;
+        }
+        inOrder(node.left, list);
+        list.add(node.val);
+        inOrder(node.right, list);
+    }
+
+    /**
+     * 非递归方式
+     * @param root
+     * @return
+     */
+    public List<Integer> inorderTraversal2(TreeNode root) {
+        List<Integer> res = new LinkedList<>();
+        Deque<TreeNode> stack = new LinkedList<>();
+        TreeNode node = root;
+        while (!stack.isEmpty() || node != null) {
+            while (node != null) {
+                stack.push(node);
+                node = node.left;
+            }
+            node = stack.pop();
+            res.add(node.val);
+            node = node.right;
+        }
+
+        return res;
+    }
+
+}
