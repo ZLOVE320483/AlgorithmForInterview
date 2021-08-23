5991d07d62a7471064793c3a2b1cc0e26fedc1b3
zlove.zhang <zlove.zhang@bytedance.com>
2021-08-23 15:39:39 +0800

    new study: 二叉查找树的第K个节点

--- src/com/zlove/practice/Main.java
+++ src/com/zlove/practice/Main.java
@@ -1,13 +1,25 @@
 package com.zlove.practice;
 
 
-import com.zlove.practice.array.OddEvenOrder;
+import com.zlove.practice.tree.KthLargest;
+import com.zlove.practice.tree.TreeNode;
 import com.zlove.practice.utils.PrintUtils;
+
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] a = {1, 2, 3, 4, 5, 6};
-        PrintUtils.printList(OddEvenOrder.oddEvenOrder(a));
+        TreeNode root = new TreeNode(3);
+        TreeNode node1 = new TreeNode(1);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node2 = new TreeNode(2);
+
+        node1.right = node2;
+        root.left = node1;
+        root.right = node4;
+
+        List<Integer> res = new KthLargest().inOrderTrans(root);
+        PrintUtils.printList(res);
     }
 }
---
+++ src/com/zlove/practice/tree/KthLargest.java
@@ -0,0 +1,54 @@
+package com.zlove.practice.tree;
+
+import java.util.Deque;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * 输入: root = [3,1,4,null,2], k = 1
+ *    3
+ *   / \
+ *  1   4
+ *   \
+ *    2
+ * 输出: 4
+ * 示例 2:
+ *
+ * 输入: root = [5,3,6,2,4,null,null,1], k = 3
+ *        5
+ *       / \
+ *      3   6
+ *     / \
+ *    2   4
+ *   /
+ *  1
+ * 输出: 4
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/er-cha-sou-suo-shu-de-di-kda-jie-dian-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class KthLargest {
+
+    public int kthLargest(TreeNode root, int k) {
+        List<Integer> res = inOrderTrans(root);
+        return res.get(res.size() - k);
+    }
+
+    public List<Integer> inOrderTrans(TreeNode node) {
+        Deque<TreeNode> stack = new LinkedList<>();
+        List<Integer> list = new LinkedList<>();
+        TreeNode cur = node;
+        while (!stack.isEmpty() || cur != null) {
+            while (cur != null) {
+                stack.push(cur);
+                cur = cur.left;
+            }
+            cur = stack.pop();
+            list.add(cur.val);
+            cur = cur.right;
+        }
+        return list;
+    }
+}
