42a9480561c8a5ad4e60597d14e95e264f786247
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-01 16:42:52 +0800

    new study: 从尾到头打印链表

---
+++ src/com/zlove/practice/listnode/ReversePrint.java
@@ -0,0 +1,24 @@
+package com.zlove.practice.listnode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 输入一个链表的头节点，从尾到头反过来返回每个节点的值（用数组返回）。
+ */
+public class ReversePrint {
+
+    public int[] reversePrint(ListNode head) {
+        List<Integer> nodes = new ArrayList<>();
+        ListNode cursor = head;
+        while (cursor != null) {
+            nodes.add(cursor.val);
+            cursor = cursor.next;
+        }
+        int[] res = new int[nodes.size()];
+        for (int i = nodes.size() - 1, j = 0; i >= 0; i--, j++) {
+            res[j] = nodes.get(i);
+        }
+        return res;
+    }
+}
