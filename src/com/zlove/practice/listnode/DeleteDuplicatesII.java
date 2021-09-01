add6466e9d3793b6c77aea5e31f2f6bac78ecdbf
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-01 17:17:33 +0800

    new study: 删除链表中重复元素

---
+++ src/com/zlove/practice/listnode/DeleteDuplicatesII.java
@@ -0,0 +1,32 @@
+package com.zlove.practice.listnode;
+
+/**
+ * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除链表中所有存在数字重复情况的节点，只保留原始链表中 没有重复出现 的数字。
+ *
+ * 返回同样按升序排列的结果链表。
+ *
+ *
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/remove-duplicates-from-sorted-list-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DeleteDuplicatesII {
+
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null && cur.next.next != null) {
+            if (cur.next.val == cur.next.next.val) {
+                int x = cur.next.val;
+                while (cur.next != null && cur.next.val == x) {
+                    cur.next = cur.next.next;
+                }
+            } else {
+                cur = cur.next;
+            }
+        }
+        return dummy.next;
+    }
+}
