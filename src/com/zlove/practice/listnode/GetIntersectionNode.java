e368934db6520a5f19003833d6583a4edb8124a0
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-03 18:05:21 +0800

    new study: 两个链表中的第一个公共节点

---
+++ src/com/zlove/practice/listnode/GetIntersectionNode.java
@@ -0,0 +1,22 @@
+package com.zlove.practice.listnode;
+
+/**
+ * 输入两个链表，找出它们的第一个公共节点。
+ *
+ * https://leetcode-cn.com/problems/liang-ge-lian-biao-de-di-yi-ge-gong-gong-jie-dian-lcof/
+ */
+public class GetIntersectionNode {
+
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        if (headA == null || headB == null) {
+            return null;
+        }
+        ListNode p1 = headA;
+        ListNode p2 = headB;
+        while (p1 != p2) {
+            p1 = p1 == null ? headB : p1.next;
+            p2 = p2 == null ? headA : p2.next;
+        }
+        return p1;
+    }
+}
