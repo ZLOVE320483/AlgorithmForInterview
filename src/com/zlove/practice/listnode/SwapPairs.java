da363844cead9590ca5becaa53e8be63efda2ef3
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-04 20:07:06 +0800

    new study: 两两交换链表中相邻的两个节点

---
+++ src/com/zlove/practice/listnode/SwapPairs.java
@@ -0,0 +1,21 @@
+package com.zlove.practice.listnode;
+
+/**
+ *给定一个链表，两两交换其中相邻的节点，并返回交换后的链表。
+ *
+ * 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+ *
+ * https://leetcode-cn.com/problems/swap-nodes-in-pairs/
+ */
+public class SwapPairs {
+
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        ListNode newHead = head.next;
+        head.next = swapPairs(newHead.next);
+        newHead.next = head;
+        return newHead;
+    }
+}
