52dd96f8a5bba5a1a3b32658aff19d7754ec169d
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-07 20:20:23 +0800

    new study: 两数之和

---
+++ src/com/zlove/practice/listnode/AddTwoNumbers.java
@@ -0,0 +1,55 @@
+package com.zlove.practice.listnode;
+
+import java.util.Stack;
+
+/**
+ *
+ * 给你两个 非空 链表来代表两个非负整数。数字最高位位于链表开始位置。它们的每个节点只存储一位数字。将这两数相加会返回一个新的链表。
+ *
+ * 你可以假设除了数字 0 之外，这两个数字都不会以零开头。
+ *
+ *
+ *
+ * 示例1：
+ *
+ *
+ *
+ * 输入：l1 = [7,2,4,3], l2 = [5,6,4]
+ * 输出：[7,8,0,7]
+ * 示例2：
+ *
+ * 输入：l1 = [2,4,3], l2 = [5,6,4]
+ * 输出：[8,0,7]
+ * 示例3：
+ *
+ * 输入：l1 = [0], l2 = [0]
+ * 输出：[0]
+ */
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Stack<Integer> stack1 = buildStack(l1);
+        Stack<Integer> stack2 = buildStack(l2);
+        ListNode head = new ListNode(-1);
+        int carry = 0;
+        while (!stack1.isEmpty() || !stack2.isEmpty() || carry != 0) {
+            int x = stack1.isEmpty() ? 0 : stack1.pop();
+            int y = stack2.isEmpty() ? 0 : stack2.pop();
+            int sum = x + y + carry;
+            ListNode node = new ListNode(sum % 10);
+            node.next = head.next;
+            head.next = node;
+            carry = sum / 10;
+        }
+        return head.next;
+    }
+
+    private Stack<Integer> buildStack(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        return stack;
+    }
+}
