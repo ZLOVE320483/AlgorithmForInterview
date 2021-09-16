d7ee37a59183545aba38900149e812f3fe688a44
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-16 15:45:24 +0800

    new study: 回文链表

---
+++ src/com/zlove/practice/listnode/IsPalindrome.java
@@ -0,0 +1,42 @@
+package com.zlove.practice.listnode;
+
+/**
+ * 给你一个单链表的头节点 head ，请你判断该链表是否为回文链表。如果是，返回 true ；否则，返回 false 。
+ *
+ * https://leetcode-cn.com/problems/palindrome-linked-list/
+ */
+public class IsPalindrome {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        fast = reverse(slow);
+        while (fast != null) {
+            if (fast.val != head.val) {
+                return false;
+            }
+            fast = fast.next;
+            head = head.next;
+        }
+        return true;
+    }
+
+    private ListNode reverse(ListNode head) {
+        ListNode cur = head;
+        ListNode pre = null;
+        while (cur != null) {
+            ListNode tmp = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = tmp;
+        }
+        return pre;
+    }
+}
