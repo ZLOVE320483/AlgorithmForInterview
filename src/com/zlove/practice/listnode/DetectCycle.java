150e5b849ea64c1c850165d3383ffb47257129be
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-17 18:31:27 +0800

    new study: 环形列表的入口

---
+++ src/com/zlove/practice/listnode/DetectCycle.java
@@ -0,0 +1,61 @@
+package com.zlove.practice.listnode;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * 给定一个链表，返回链表开始入环的第一个节点。 如果链表无环，则返回 null。
+ *
+ * 为了表示给定链表中的环，我们使用整数 pos 来表示链表尾连接到链表中的位置（索引从 0 开始）。 如果 pos 是 -1，则在该链表中没有环。注意，pos 仅仅是用于标识环的情况，并不会作为参数传递到函数中。
+ *
+ * 说明：不允许修改给定的链表。
+ *
+ * 进阶：
+ *
+ * 你是否可以使用 O(1) 空间解决此题？
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DetectCycle {
+
+    public ListNode detectCycle(ListNode head) {
+        Set<ListNode> set = new HashSet<>();
+        ListNode cur = head;
+        while (cur != null) {
+            if (set.contains(cur)) {
+                return cur;
+            } else {
+                set.add(cur);
+            }
+            cur = cur.next;
+        }
+        return null;
+    }
+
+    public ListNode detectCycle2(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = head;
+        boolean hasCycle = false;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                hasCycle = true;
+                break;
+            }
+        }
+        if (hasCycle) {
+            ListNode q = head;
+            while (slow != q) {
+                slow = slow.next;
+                q = q.next;
+            }
+            return q;
+        } else {
+            return null;
+        }
+    }
+}
