a585d3582504ba18dd301517016a220456f2e5fb
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-18 16:56:48 +0800

    new study: 和为S的两个数

---
+++ src/com/zlove/practice/two_pointer/TwoSum.java
@@ -0,0 +1,41 @@
+package com.zlove.practice.two_pointer;
+
+/**
+ * 输入一个递增排序的数组和一个数字s，在数组中查找两个数，使得它们的和正好是s。如果有多对数字的和等于s，则输出任意一对即可。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：nums = [2,7,11,15], target = 9
+ * 输出：[2,7] 或者 [7,2]
+ * 示例 2：
+ *
+ * 输入：nums = [10,26,30,31,47,60], target = 40
+ * 输出：[10,30] 或者 [30,10]
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/he-wei-sde-liang-ge-shu-zi-lcof
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
+        int i = 0;
+        int j = nums.length - 1;
+        while (i < j) {
+            int sum = nums[i] + nums[j];
+            if (sum == target) {
+                res[0] = nums[i];
+                res[1] = nums[j];
+                break;
+            } else if (sum < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return res;
+    }
+}
