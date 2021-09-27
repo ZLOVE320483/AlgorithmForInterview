e7d0bc1ad0d801cdf3bcb05f3a39b83facbcb3ed
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-27 14:13:01 +0800

    new study: 回文字串的数目

---
+++ src/com/zlove/practice/string/CountSubstrings.java
@@ -0,0 +1,34 @@
+package com.zlove.practice.string;
+
+/**
+ * 给你一个字符串 s ，请你统计并返回这个字符串中 回文子串 的数目。
+ *
+ * 回文字符串 是正着读和倒过来读一样的字符串。
+ *
+ * 子字符串 是字符串中的由连续字符组成的一个序列。
+ *
+ * 具有不同开始位置或结束位置的子串，即使是由相同的字符组成，也会被视作不同的子串。
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/palindromic-substrings
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class CountSubstrings {
+
+    int result = 0;
+    public int countSubstrings(String s) {
+        for (int i = 0; i < s.length(); i++) {
+            extendSubstrings(s, i, i);
+            extendSubstrings(s, i, i + 1);
+        }
+        return result;
+    }
+
+    private void extendSubstrings(String s, int start, int end) {
+        while (start >= 0 && end < s.length() && s.charAt(start) == s.charAt(end)) {
+            result++;
+            start--;
+            end++;
+        }
+    }
+}
