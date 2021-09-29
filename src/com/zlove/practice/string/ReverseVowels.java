d2cb08bbc726c890fdb9b28860143aa508cacdc1
zlove.zhang <zlove.zhang@bytedance.com>
2021-09-29 19:49:50 +0800

    new study: 反转字符串中元音字符

---
+++ src/com/zlove/practice/string/ReverseVowels.java
@@ -0,0 +1,53 @@
+package com.zlove.practice.string;
+
+import java.util.Arrays;
+import java.util.HashSet;
+
+/**
+ * 给你一个字符串 s ，仅反转字符串中的所有元音字母，并返回结果字符串。
+ *
+ * 元音字母包括 'a'、'e'、'i'、'o'、'u'，且可能以大小写两种形式出现。
+ *
+ *  
+ *
+ * 示例 1：
+ *
+ * 输入：s = "hello"
+ * 输出："holle"
+ * 示例 2：
+ *
+ * 输入：s = "leetcode"
+ * 输出："leotcede"
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/reverse-vowels-of-a-string
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class ReverseVowels {
+
+    private final static HashSet<Character> vowels = new HashSet<>(
+            Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+    public String reverseVowels(String s) {
+        if (s == null) {
+            return null;
+        }
+
+        char[] result = new char[s.length()];
+        int i = 0;
+        int j = s.length() - 1;
+        while (i <= j) {
+            char ci = s.charAt(i);
+            char cj = s.charAt(j);
+            if (!vowels.contains(ci)) {
+                result[i++] = ci;
+            } else if (!vowels.contains(cj)) {
+                result[j--] = cj;
+            } else {
+                result[i++] = cj;
+                result[j--] = ci;
+            }
+        }
+        return new String(result);
+    }
+}
