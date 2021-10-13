11f4933fee2f9ec72cd5eaa72ceeee3927fe5425
zlove.zhang <zlove.zhang@bytedance.com>
2021-10-13 17:29:45 +0800

    new study: 为表达式设计优先级

---
+++ src/com/zlove/practice/string/DiffWaysToCompute.java
@@ -0,0 +1,72 @@
+package com.zlove.practice.string;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 给定一个含有数字和运算符的字符串，为表达式添加括号，改变其运算优先级以求出不同的结果。你需要给出所有可能的组合的结果。有效的运算符号包含 +, - 以及 * 。
+ *
+ * 示例 1:
+ *
+ * 输入: "2-1-1"
+ * 输出: [0, 2]
+ * 解释:
+ * ((2-1)-1) = 0
+ * (2-(1-1)) = 2
+ * 示例 2:
+ *
+ * 输入: "2*3-4*5"
+ * 输出: [-34, -14, -10, -10, 10]
+ * 解释:
+ * (2*(3-(4*5))) = -34
+ * ((2*3)-(4*5)) = -14
+ * ((2*(3-4))*5) = -10
+ * (2*((3-4)*5)) = -10
+ * (((2*3)-4)*5) = 10
+ *
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode-cn.com/problems/different-ways-to-add-parentheses
+ * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+public class DiffWaysToCompute {
+
+    private final Map<String, List<Integer>> resultMap = new HashMap<>();
+
+    public List<Integer> diffWaysToCompute(String expression) {
+        if (resultMap.containsKey(expression)) {
+            return resultMap.get(expression);
+        }
+        List<Integer> result = new LinkedList<>();
+        for (int i = 0; i < expression.length(); i++) {
+            char c = expression.charAt(i);
+            if (c == '+' || c == '-' || c == '*') {
+                List<Integer> left = diffWaysToCompute(expression.substring(0, i));
+                List<Integer> right = diffWaysToCompute(expression.substring(i + 1));
+                for (int l : left) {
+                    for (int r : right) {
+                        switch (c) {
+                            case '+':
+                                result.add(l + r);
+                                break;
+                            case '-':
+                                result.add(l - r);
+                                break;
+                            case '*':
+                                result.add(l * r);
+                                break;
+                        }
+                    }
+                }
+            }
+        }
+        if (result.isEmpty()) {
+            result.add(Integer.valueOf(expression));
+        }
+        resultMap.put(expression, result);
+        return result;
+    }
+
+
+}
