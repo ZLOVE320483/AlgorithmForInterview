fad1435ac77660bbc0c0906099cac90fede08db1
zlove.zhang <zlove.zhang@bytedance.com>
2022-02-10 10:21:07 +0800

    算法再复习

--- src/com/zlove/practice/Main.java
+++ src/com/zlove/practice/Main.java
@@ -5,21 +5,29 @@ import com.zlove.practice.tree.KthLargest;
 import com.zlove.practice.tree.TreeNode;
 import com.zlove.practice.utils.PrintUtils;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) {
-        TreeNode root = new TreeNode(3);
-        TreeNode node1 = new TreeNode(1);
-        TreeNode node4 = new TreeNode(4);
-        TreeNode node2 = new TreeNode(2);
+        int[] nums = {3, 2, 4};
+        PrintUtils.printList(twoSum(nums, 6));
+    }
 
-        node1.right = node2;
-        root.left = node1;
-        root.right = node4;
 
-        List<Integer> res = new KthLargest().inOrderTrans(root);
-        PrintUtils.printList(res);
+    public static int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
+        for (int i = 0; i < nums.length; i++) {
+            int value = map.get(target - nums[i]);
+            if (map.containsKey(target - nums[i]) && value != i) {
+                return new int[] {i, value};
+            }
+        }
+        return new int[2];
     }
 }
--- src/com/zlove/practice/tree/FindPath.java
+++ src/com/zlove/practice/tree/FindPath.java
@@ -27,9 +27,6 @@ import java.util.List;
  * 链接：https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof
  * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/er-cha-shu-zhong-he-wei-mou-yi-zhi-de-lu-jing-lcof
- * 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
  */
 public class FindPath {
 
