58bb63380b39b93aab2cec2715d7af73ccb82fef
zlove.zhang <zlove.zhang@bytebance.com>
2022-02-17 18:32:52 +0800

    算法再复习

--- src/com/zlove/practice/Main.java
+++ src/com/zlove/practice/Main.java
@@ -1,33 +1,52 @@
 package com.zlove.practice;
 
 
-import com.zlove.practice.tree.KthLargest;
 import com.zlove.practice.tree.TreeNode;
 import com.zlove.practice.utils.PrintUtils;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {3, 2, 4};
-        PrintUtils.printList(twoSum(nums, 6));
+        TreeNode root = new TreeNode(1);
+        TreeNode node2 = new TreeNode(2);
+        TreeNode node3 = new TreeNode(3);
+        TreeNode node4 = new TreeNode(4);
+        TreeNode node5 = new TreeNode(5);
+
+        root.left = node2;
+        root.right = node3;
+        node2.right = node5;
+        node3.right = node4;
+
+        List<Integer> res = rightSideView(root);
+        PrintUtils.printList(res);
     }
 
 
-    public static int[] twoSum(int[] nums, int target) {
-        Map<Integer, Integer> map = new HashMap();
-        for (int i = 0; i < nums.length; i++) {
-            map.put(nums[i], i);
+    public static List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new LinkedList();
+        if (root == null) {
+            return res;
         }
-        for (int i = 0; i < nums.length; i++) {
-            int value = map.get(target - nums[i]);
-            if (map.containsKey(target - nums[i]) && value != i) {
-                return new int[] {i, value};
+        Deque<TreeNode> stack = new LinkedList();
+        stack.offer(root);
+        while(!stack.isEmpty()) {
+            int size = stack.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode node = stack.poll();
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+                if (node.left != null) {
+                    stack.offer(node.left);
+                }
+                if (node.right != null) {
+                    stack.offer(node.right);
+                }
             }
         }
-        return new int[2];
+        return res;
     }
 }
