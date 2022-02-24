2e3a58631dd369dfe751883a8385fe3212d09603
zlove.zhang <zlove.zhang@bytedance.com>
2022-02-24 20:59:03 +0800

    复习算法

--- leetcode/editor/cn/[538]把二叉搜索树转换为累加树.java
+++ leetcode/editor/cn/[538]把二叉搜索树转换为累加树.java
@@ -72,8 +72,20 @@ sum-tree/ 相同
  * }
  */
 class Solution {
+    int sum = 0;
     public TreeNode convertBST(TreeNode root) {
+        convert(root);
+        return root;
+    }
 
+    private void convert(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        convert(root.right);
+        sum += root.val;
+        root.val = sum;
+        convert(root.left);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
