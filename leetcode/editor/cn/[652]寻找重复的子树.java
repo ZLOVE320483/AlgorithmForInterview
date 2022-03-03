616ef3d35ce85d10b048acfa2fb5573dd14d618c
zlove.zhang <zlove.zhang@bytedance.com>
2022-03-03 16:22:23 +0800

    复习算法

---
+++ leetcode/editor/cn/[652]寻找重复的子树.java
@@ -0,0 +1,85 @@
+/**
+给定一棵二叉树 root，返回所有重复的子树。 
+
+ 对于同一类的重复子树，你只需要返回其中任意一棵的根结点即可。 
+
+ 如果两棵树具有相同的结构和相同的结点值，则它们是重复的。 
+
+ 
+
+ 示例 1： 
+
+ 
+
+ 
+输入：root = [1,2,3,4,null,2,4,null,null,4]
+输出：[[2,4],[4]] 
+
+ 示例 2： 
+
+ 
+
+ 
+输入：root = [2,1,1]
+输出：[[1]] 
+
+ 示例 3： 
+
+ 
+
+ 
+输入：root = [2,2,2,3,null,3,null]
+输出：[[2,3],[3]] 
+
+ 
+
+ 提示： 
+
+ 
+ 树中的结点数在[1,10^4]范围内。 
+ -200 <= Node.val <= 200 
+ 
+ Related Topics 树 深度优先搜索 哈希表 二叉树 👍 374 👎 0
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    Map<String, Integer> treeCount = new HashMap();
+    List<TreeNode> res = new LinkedList();
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        transTree(root);
+        return res;
+    }
+
+    private String transTree(TreeNode root) {
+        if (root == null) {
+            return "#";
+        }
+        String leftTree = transTree(root.left);
+        String rightTree = transTree(root.right);
+        String treeStr = root.val + "," + leftTree + "," + rightTree;
+        int count = treeCount.getOrDefault(treeStr, 0);
+        if (count == 1) {
+            res.add(root);
+        }
+        treeCount.put(treeStr, count + 1);
+        return treeStr;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
