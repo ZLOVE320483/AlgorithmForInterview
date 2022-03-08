19823f62de80057f1bf8d66122316f8032f1a3a8
zlove.zhang <zlove.zhang@bytedance.com>
2022-03-08 20:10:34 +0800

    复习算法

--- leetcode/editor/cn/[671]二叉树中第二小的节点.java
+++ leetcode/editor/cn/[671]二叉树中第二小的节点.java
@@ -56,8 +56,27 @@
  * }
  */
 class Solution {
+    int ans;
+    int rootVal;
     public int findSecondMinimumValue(TreeNode root) {
+        ans = -1;
+        rootVal = root.val;
+        dfs(root);
+        return ans;
+    }
 
+    private void dfs(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        if (ans != -1 && root.val >= ans) {
+            return;
+        }
+        if (root.val > rootVal) {
+            ans = root.val;
+        }
+        dfs(root.left);
+        dfs(root.right);
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
