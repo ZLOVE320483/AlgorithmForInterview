b4b6b30b3880b21bffd6be4c59817e5c48c72004
zlove.zhang <zlove.zhang@bytedance.com>
2022-03-30 19:41:43 +0800

    滑动数组

--- src/com/zlove/practice/Main.java
+++ src/com/zlove/practice/Main.java
@@ -8,9 +8,24 @@ public class Main {
     Deque<Integer> path = new LinkedList();
 
     public static void main(String[] args) {
-        String a = "a" + "b";
-        String b = "ab";
-        System.out.println(a == b);
+        int[] nums = {2,3,1,2,4,3};
+        System.out.println(minSubArrayLen(7, nums));
+    }
+
+    public static int minSubArrayLen(int target, int[] nums) {
+        int result = Integer.MAX_VALUE;
+        int left = 0, sum = 0;
+        for (int right = 0; right < nums.length; right++) {
+            sum += nums[right];
+            if (sum == target) {
+                result = Math.min(result, right - left + 1);
+            }
+            while (sum > target) {
+                sum -= nums[left];
+                left++;
+            }
+        }
+        return result;
     }
 
 
