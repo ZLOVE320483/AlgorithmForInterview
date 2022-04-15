f935ba9fb7233f021dd62ba9004ecfd1fd8fc1b9
zlove.zhang <zlove.zhang@bytedance.com>
2022-04-15 21:08:45 +0800

    重复的子字符串

---
+++ leetcode/editor/cn/[459]重复的子字符串.java
@@ -0,0 +1,68 @@
+/**
+给定一个非空的字符串 s ，检查是否可以通过由它的一个子串重复多次构成。 
+
+ 
+
+ 示例 1: 
+
+ 
+输入: s = "abab"
+输出: true
+解释: 可由子串 "ab" 重复两次构成。
+ 
+
+ 示例 2: 
+
+ 
+输入: s = "aba"
+输出: false
+ 
+
+ 示例 3: 
+
+ 
+输入: s = "abcabcabcabc"
+输出: true
+解释: 可由子串 "abc" 重复四次构成。 (或子串 "abcabc" 重复两次构成。)
+ 
+
+ 
+
+ 提示： 
+
+ 
+
+ 
+ 1 <= s.length <= 10⁴ 
+ s 由小写英文字母组成 
+ 
+ Related Topics 字符串 字符串匹配 👍 675 👎 0
+
+*/
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        if (s.length() == 1) {
+            return false;
+        }
+        int sLen = s.length();
+        for (int i = 1; i <= sLen / 2; i++) {
+            String sub = s.substring(0, i);
+            int len = sub.length();
+            if (sLen % len != 0) {
+                continue;
+            }
+            int count = sLen / len;
+            StringBuilder builder = new StringBuilder();
+            for (int j = 0; j < count; j++) {
+                builder.append(sub);
+            }
+            if (s.equals(builder.toString())) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
